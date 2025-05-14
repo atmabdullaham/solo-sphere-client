94438c254859a7b71b3db3ff3ff137c520e40a07
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-05-14 09:32:26 +0600

    posted jobs and show jobs on all-jobs routes and show also home page based on category

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -10,8 +10,8 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_projectId,
   storageBucket: import.meta.env.VITE_storageBucket,
   messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig); 
